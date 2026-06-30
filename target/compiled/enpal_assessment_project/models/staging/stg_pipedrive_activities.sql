9fe95fadf0b40857268523c818ea8309ffaf1df2
Jimmy Pang <davnins2003@gmail.com>
2026-06-30 11:17:17 +0200

    refactor: split due_at into distinct due_at_utc and due_at_berlin columns in stg_pipedrive_activities

--- target/compiled/enpal_assessment_project/models/staging/stg_pipedrive_activities.sql
+++ target/compiled/enpal_assessment_project/models/staging/stg_pipedrive_activities.sql
@@ -13,14 +13,15 @@ WITH
             cast(assigned_to_user AS integer) AS assigned_user_id,
             cast(deal_id AS integer) AS deal_id,
             cast(done AS boolean) AS is_done,
-            cast(due_to AS timestamp) AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Berlin' AS due_at
+            cast(due_to AS timestamp) AS due_at_utc,
+            cast(due_to AS timestamp) AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Berlin' AS due_at_berlin
         FROM
             source
     ),
     deduplicated AS (
         SELECT
             *,
-            row_number() OVER (PARTITION BY activity_id ORDER BY due_at DESC) AS row_num
+            row_number() OVER (PARTITION BY activity_id ORDER BY due_at_utc DESC) AS row_num
         FROM
             renamed
     )
@@ -30,7 +31,8 @@ SELECT
     assigned_user_id,
     deal_id,
     is_done,
-    due_at
+    due_at_utc,
+    due_at_berlin
 FROM
     deduplicated
 WHERE
