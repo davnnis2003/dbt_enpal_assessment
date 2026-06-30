5a6d8692789a005d53331788d6cd6713c7b1ef17
Jimmy Pang <davnins2003@gmail.com>
2026-06-30 11:07:09 +0200

    refactor: add explicit aliases to stg_pipedrive_fields and stg_pipedrive_activity_types dbt models

--- models/staging/stg_pipedrive_fields.sql
+++ models/staging/stg_pipedrive_fields.sql
@@ -1,20 +1,29 @@
-{{ config(
-    materialized='view',
-    schema='staging'
-) }}
-
-with source as (
-    select * from {{ source('postgres_public', 'fields') }}
-),
-
-renamed as (
-    select
-        cast(id as integer) as field_id,
-        cast(field_key as varchar) as field_key,
-        cast(name as varchar) as field_name,
-        field_value_options
-
-    from source
-)
-
-select * from renamed
+{
+    {
+        config (
+            materialized = 'view',
+            schema = 'staging',
+            alias = 'stg_pipedrive_fields'
+        )
+    }
+}
+WITH
+    source AS (
+        SELECT
+            *
+        FROM
+            {{source ('postgres_public', 'fields')}}
+    ),
+    renamed AS (
+        SELECT
+            cast(id AS integer) AS field_id,
+            cast(field_key AS varchar) AS field_key,
+            cast(name AS varchar) AS field_name,
+            field_value_options
+        FROM
+            source
+    )
+SELECT
+    *
+FROM
+    renamed
--- target/compiled/enpal_assessment_project/models/staging/stg_pipedrive_activity_types.sql
+++ target/compiled/enpal_assessment_project/models/staging/stg_pipedrive_activity_types.sql
@@ -1,21 +1,33 @@
-
-
-with source as (
-    select * from "postgres"."public"."activity_types"
-),
-
-renamed as (
-    select
-        cast(id as integer) as activity_type_id,
-        cast(name as varchar) as activity_type_name,
-        case
-            when active = 'Yes' then true
-            when active = 'No' then false
-            else null
-        end as is_active,
-        cast(type as varchar) as activity_type_category
-
-    from source
-)
-
-select * from renamed
+{
+    {
+        config (
+            materialized = 'view',
+            schema = 'staging',
+            alias = 'stg_pipedrive_activity_types'
+        )
+    }
+}
+WITH
+    source AS (
+        SELECT
+            *
+        FROM
+            "postgres"."public"."activity_types"
+    ),
+    renamed AS (
+        SELECT
+            cast(id AS integer) AS activity_type_id,
+            cast(name AS varchar) AS activity_type_name,
+            CASE
+                WHEN active = 'Yes' THEN TRUE
+                WHEN active = 'No' THEN FALSE
+                ELSE NULL
+            END AS is_active,
+            cast(type AS varchar) AS activity_type_category
+        FROM
+            source
+    )
+SELECT
+    *
+FROM
+    renamed
