d47c747bc7e1fbb6830c998764c451406edf0551
Jimmy Pang <davnins2003@gmail.com>
2026-06-30 10:49:53 +0200

    feat: add staging model for activity types with schema tests

---
+++ models/staging/staging__activity_types.sql
@@ -0,0 +1,19 @@
+with source as (
+    select * from {{ source('postgres_public', 'activity_types') }}
+),
+
+renamed as (
+    select
+        cast(id as integer) as activity_type_id,
+        cast(name as varchar) as activity_type_name,
+        case
+            when active = 'Yes' then true
+            when active = 'No' then false
+            else null
+        end as is_active,
+        cast(type as varchar) as activity_type_category
+
+    from source
+)
+
+select * from renamed
---
+++ target/compiled/enpal_assessment_project/models/staging/configs/staging__activity_types.yml/not_null_staging__activity_types_activity_type_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select activity_type_id
+from "postgres"."public_staging"."staging__activity_types"
+where activity_type_id is null
+
+
