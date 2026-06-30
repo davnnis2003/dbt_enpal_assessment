72392fbdccdd895a0bed02c91ef885a313cb9430
Jimmy Pang <davnins2003@gmail.com>
2026-06-30 11:14:16 +0200

    feat: add stg_pipedrive_activities staging model with unique and not-null constraints

---
+++ models/staging/stg_pipedrive_activities.sql
@@ -0,0 +1,41 @@
+{{ config(
+    materialized='view',
+    schema='staging',
+    alias='stg_pipedrive_activities'
+) }}
+WITH
+    source AS (
+        SELECT
+            *
+        FROM
+            {{ source('postgres_public', 'activity') }}
+    ),
+    renamed AS (
+        SELECT
+            cast(activity_id AS integer) AS activity_id,
+            cast(type AS varchar) AS activity_type_category,
+            cast(assigned_to_user AS integer) AS assigned_user_id,
+            cast(deal_id AS integer) AS deal_id,
+            cast(done AS boolean) AS is_done,
+            cast(due_to AS timestamp) AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Berlin' AS due_at
+        FROM
+            source
+    ),
+    deduplicated AS (
+        SELECT
+            *,
+            row_number() OVER (PARTITION BY activity_id ORDER BY due_at DESC) AS row_num
+        FROM
+            renamed
+    )
+SELECT
+    activity_id,
+    activity_type_category,
+    assigned_user_id,
+    deal_id,
+    is_done,
+    due_at
+FROM
+    deduplicated
+WHERE
+    row_num = 1
